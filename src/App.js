a7f67dd728656dbaac0c4e4daf4e1938a8c56d64
Arshadvk <arshadvk7560@gmail.com>
2023-06-07 14:20:13 +0530

    olx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {  BrowserRouter , Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
-
+import Signup from './Pages/Signup'
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes  >
+
+
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/signup' element={<Signup />}/>
+        
+        
+        
+        </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase' ;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIhMi3e6tioTj9a_0cLIpsJCrOSPofl-g",
+    authDomain: "olx-clone-a4962.firebaseapp.com",
+    projectId: "olx-clone-a4962",
+    storageBucket: "olx-clone-a4962.appspot.com",
+    messagingSenderId: "454259968766",
+    appId: "1:454259968766:web:afb262725fa2ce583333af",
+    measurementId: "G-YNJRJ0G91E"
+  };
+ export default firebase.initializeApp(firebaseConfig)
